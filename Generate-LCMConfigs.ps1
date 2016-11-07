3a68da8d1f29840f7a5fa1d8da0feea766ab2c20
joeypiccola <joey.piccola@gmail.com>
2016-11-07 09:38:56 -0700

    fixed spaces

--- Generate-LCMConfigs.ps1
+++ Generate-LCMConfigs.ps1
@@ -5,21 +5,21 @@ Configuration DFS_LCM_Config
     {
         Settings
         {
-            RefreshFrequencyMins            = 30;
-            RefreshMode                     = "PULL";
-            ConfigurationMode               ="ApplyAndAutocorrect";
-            AllowModuleOverwrite            = $true;
-            RebootNodeIfNeeded              = $true;
-            ConfigurationModeFrequencyMins  = 15;
-            CertificateID                   = $node.thumbprint
-            ActionAfterReboot               = 'ContinueConfiguration'
+            RefreshFrequencyMins           = 30;
+            RefreshMode                    = "PULL";
+            ConfigurationMode              ="ApplyAndAutocorrect";
+            AllowModuleOverwrite           = $true;
+            RebootNodeIfNeeded             = $true;
+            ConfigurationModeFrequencyMins = 15;
+            CertificateID                  = $node.thumbprint
+            ActionAfterReboot              = 'ContinueConfiguration'
         }
         ConfigurationRepositoryWeb jpiPullServer
         {
-            ServerURL                       = 'https://dscpull01.ad.piccola.us:8080/PSDSCPullServer.svc'    
-            RegistrationKey                 = '2b3fa01b-9ee4-452d-94c3-2f637502e761'  
-            ConfigurationNames              = $node.nodename
-        }     
+            ServerURL          = 'https://dscpull01.ad.piccola.us:8080/PSDSCPullServer.svc'
+            RegistrationKey    = '2b3fa01b-9ee4-452d-94c3-2f637502e761'
+            ConfigurationNames = $node.nodename
+        }
     }
 }
 
